350b56a0438ac1c6e90870dc60a5ba9fb497e031
Cao Li <tsowly@hotmail.com>
2013-03-18 00:15:47 +0800

    skeleton

--- lib/pichuli.rb
+++ lib/pichuli.rb
@@ -1,5 +1,28 @@
 require "pichuli/version"
 
 module Pichuli
-  # Your code goes here...
+  module Worker
+  	# Public
+  	# return nil
+  	def perform_single(*args)
+  		
+  	end
+
+  	# Public
+  	# return nil
+  	def perform_batch(*args)
+  		args.each do |arg|
+  			perform_single(arg)
+  		end
+  	end
+
+  	def self.included(base)
+  		base.extend(ClassMethods)
+  	end
+  	module ClassMethods
+  		def perform(*args)
+  			#enqueue task
+  		end
+  	end
+  end
 end
